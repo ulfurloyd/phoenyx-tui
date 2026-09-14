d60cb2ca55cd508338b83383c28bcf591224edbd
ulfurloyd <wolfinthedepths@proton.me>
2026-09-14 18:13:32 +0530

    feat: basic choices

--- main.go
+++ main.go
@@ -5,7 +5,20 @@ import (
 	"fmt"
 )
 
-type model struct{}
+type model struct {
+	choices []string
+	cursor  int
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{
+			"SSH nyx",
+			"SSH hermes",
+			"Shell",
+		},
+	}
+}
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -15,6 +28,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyPressMsg:
 		switch msg.String() {
+		case "j", "down", "ctrl+n":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "k", "up", "ctrl+p":
+			if m.cursor > 0 {
+				m.cursor--
+			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		}
@@ -24,13 +45,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
-	v := tea.NewView("phoenyx\n\nq: quit\n")
+	s := "phoenyx\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+
+		if i == m.cursor {
+			cursor = ">"
+		}
+
+		s += cursor + " " + choice + "\n"
+	}
+
+	s += "\nq: quit"
+
+	v := tea.NewView(s)
 	v.AltScreen = true
 	return v
 }
 
 func main() {
-	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+	if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 }
