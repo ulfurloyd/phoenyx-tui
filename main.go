742ba4a542182a6d26d4d1f31e9745a24d9973a3
ulfurloyd <wolfinthedepths@proton.me>
2026-09-14 18:03:02 +0530

    feat: barebones bubbletea UI

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	tea "charm.land/bubbletea/v2"
+	"fmt"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	v := tea.NewView("phoenyx\n\nq: quit\n")
+	v.AltScreen = true
+	return v
+}
 
 func main() {
-	fmt.Println("hello world")
+	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 }
